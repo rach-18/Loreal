98b80f732c951cdd4ab3560a1cc35917a22bf097
Himanshu Sondhiya <sondhiyahimanshu786@gmail.com>
2024-03-17 23:04:44 +0530

    Finished Page HTML

---
+++ Careers/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily:{
+        Questrial : ['"Questrial"']
+      }
+    },
+  },
+  plugins: [],
+}
+
